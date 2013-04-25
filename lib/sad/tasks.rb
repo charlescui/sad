d61191152330b0a46ecf04cf20cfc5e604e20263
崔峥 <zheng.cuizh@gmail.com>
2013-04-25 23:51:20 +0800

    modify daemons arguments

--- lib/sad/tasks.rb
+++ lib/sad/tasks.rb
@@ -1,10 +1,10 @@
 namespace :sad do
-	desc "start sad with args - COUNT=4 QUEUE=sosad"
+	desc "start sad with args - COUNT=4 QUEUE=sosad DIR=./tmp/pids"
 	task :start do
 		opts = {
 			:dir => ENV['DIR'],
 			:multiple => true,
-			:mode => :exec,
+			:log_output => true
 			:backtrace  => true
 		}
 		Sad::Runner.start(opts)
